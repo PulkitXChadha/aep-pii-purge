a81d9896c07d6374b49fcf07a2bd4364d5b8429e
Pulkit Chadha <pulkit2304@gmail.com>
2021-06-07 12:35:16 -0400

    corrected project name greeting

--- web-src/src/index.js
+++ web-src/src/index.js
@@ -66,8 +66,8 @@ function bootstrapInExcShell () {
   // set solution info, shortTitle is used when window is too small to display full title
   runtime.solution = {
     icon: 'AdobeExperienceCloud',
-    title: 'TremendousPinkDonkey',
+    title: 'pii-cleanup',
     shortTitle: 'JGR'
   }
-  runtime.title = 'TremendousPinkDonkey'
+  runtime.title = 'pii-cleanup'
 }
